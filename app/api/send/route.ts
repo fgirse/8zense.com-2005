10750a9ff5fda48d5dd6179c8ab15cb214918591
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-05-10 11:32:37 +0200

    commit 5er

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,18 +1,21 @@
+import { NextRequest } from 'next/server';
 import { Resend } from 'resend';
 import * as React from 'react';
 import { ContactUsEmail } from '@/components/email/contact-us';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
+
+export async function GET(req: Request) {
+  
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const payload = await req.json();
 
   try {
     const { data, error } = await resend.emails.send({
       from: 'info@8zense.com',
-      to: payload.email,
+      to: 'fgirse@bluewin.ch',
       subject: `Vielen Dank für Ihr Interesse an 8zense.com, ${payload.name}`,
-      react: ContactUsEmail(payload),
+      html: '<h1>Vielen Dank für Ihr Interesse an 8zense.com</h1>'
     });
 
     if (error) {
